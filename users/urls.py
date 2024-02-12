5ab91cdfd3f27aac0789c46aca93fa858ef459fa
breezh <benii1030@gmail.com>
2024-02-12 18:35:35 +0100

    create basic navbar and protected page

--- users/urls.py
+++ users/urls.py
@@ -5,4 +5,5 @@ from django.contrib.auth import views as auth_view
 urlpatterns = [
     path('login/', views.user_login, name='login'),
     path('logout/', auth_view.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
+    path('', views.index, name='index'),
 ]
