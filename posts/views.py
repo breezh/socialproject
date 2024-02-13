97527f03640b03967855980fc964ab614e9e5f80
breezh <benii1030@gmail.com>
2024-02-13 18:18:54 +0100

    created feed, started implementing like

--- posts/views.py
+++ posts/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from .forms import PostCreateForm
 from django.contrib.auth.decorators import login_required
+from .models import Post
 
 
 @login_required
@@ -14,3 +15,15 @@ def post_create(request):
     else:
         form = PostCreateForm(data=request.GET)
     return render(request, 'posts/create.html', {'form':form})
+
+def feed(request):
+    posts = Post.objects.all()
+    return render(request, 'posts/feed.html', {'posts':posts})
+
+def like_post(request):
+    post_id = request.POST.get('post_id')
+    post = get_object_or_404(Post, id=post_id)
+    if post.liked_by.filter(id=request.user.id).exists():
+        pass
+    else:
+        post.liked_by.add(request.user)
